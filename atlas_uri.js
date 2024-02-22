8c90dbb3acd25ddb8dfa6e5912798742aceff54c
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-02-22 23:02:43 +0530

    init project

---
+++ atlas_uri.js
@@ -0,0 +1,3 @@
+const username = ""; // Your username
+const password = ""; // Your password
+module.exports = uri = `mongodb+srv://${username}:${password}@cluster0.vllnh4g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
